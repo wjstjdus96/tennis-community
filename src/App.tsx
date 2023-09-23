d0a1130fd0e7d9d8bb12843765a5397b73ca88ed
seoyeon <wjstjdus96@naver.com>
2023-09-24 01:41:55 +0900

    feat: 로고 생성 및 애니메이션 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Header from "./components/Header";
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,78 @@
+import styled, { keyframes } from "styled-components";
+import { BiSolidTennisBall } from "react-icons/bi";
+
+const fadeIn = keyframes`
+    from {
+		opacity: 0;
+		transform: translate3d(0, -20%, 0);
+	}
+	to {
+		opacity: 1;
+		transform: translate3d(0, 0, 0);
+	}
+`;
+
+const bounce = keyframes`
+  0% {
+    top: -60px;
+  }
+  to {
+    top:10px;
+  } 
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  @media (max-width: 767px) {
+    margin-top: 10px;
+  }
+  @media (min-width: 768px) and (max-width: 991px) {
+  }
+
+  @media (min-width: 992px) and (max-width: 1199px) {
+  }
+
+  @media (min-width: 1200px) {
+    margin-top: 150px;
+  }
+`;
+
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 10vh;
+`;
+
+const LogoText = styled.div`
+  font-size: 150px;
+  font-family: "Allan", cursive;
+  letter-spacing: 20px;
+  animation: ${fadeIn} 1s ease-in both;
+`;
+
+const LogoIcon = styled(BiSolidTennisBall)`
+  position: relative;
+  font-size: 100px;
+  animation: ${bounce} 1s ease-in Infinite Alternate;
+`;
+
+const Button = styled.div``;
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <Wrapper>
+      <Logo>
+        <LogoText>Tenning </LogoText>
+        <LogoIcon />
+      </Logo>
+      <button>로그인</button>
+      <div>
+        <p>이미 회원이신가요?</p>
+        <button>회원가입</button>
+      </div>
+    </Wrapper>
+  );
 }
