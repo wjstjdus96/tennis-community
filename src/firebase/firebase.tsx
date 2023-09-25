baffab64df3a3b4643f3a863722b02edcfb7a93a
seoyeon <wjstjdus96@naver.com>
2023-09-25 22:23:40 +0900

    feat: 파이어베이스 연결

---
+++ src/firebase/firebase.tsx
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB9aLg8dgi33Q3zLnXlTtk9T6AjjFY250M",
+  authDomain: "tennis-project-ce502.firebaseapp.com",
+  projectId: "tennis-project-ce502",
+  storageBucket: "tennis-project-ce502.appspot.com",
+  messagingSenderId: "63827663163",
+  appId: "1:63827663163:web:c7306d7bd002e65a07bf1e",
+  measurementId: "G-H1946QNGZV",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
