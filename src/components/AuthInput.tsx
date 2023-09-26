47748fa7a18ec6c8b39d9e7328cf888345161b88
seoyeon <wjstjdus96@naver.com>
2023-09-26 16:48:03 +0900

    feat: react-hook-form 사용하여 로그인 폼 기능 구현

--- src/components/AuthInput.tsx
+++ src/components/AuthInput.tsx
@@ -1,16 +1,32 @@
 import styled from "styled-components";
+import { Path, UseFormRegister } from "react-hook-form";
+import { ILoginValue } from "../pages/Login";
 
 interface IAuthInput {
-  id: string;
-  name: string;
-  value?: string;
+  name: Path<ILoginValue>;
+  text: string;
+  inputType?: string;
+  register: UseFormRegister<ILoginValue>;
+  errorMsg?: string;
 }
 
-export default function AuthInput({ id, name, value }: IAuthInput) {
+export default function AuthInput({
+  name,
+  text,
+  inputType = "text",
+  register,
+  errorMsg,
+}: IAuthInput) {
   return (
     <Wrapper>
-      <label htmlFor={id}>{id.toUpperCase()}</label>
-      <input id={id} placeholder={`${name}을 입력하십시오`} />
+      <label htmlFor={name}>{name.toUpperCase()}</label>
+      <input
+        id={name}
+        type={inputType}
+        {...register(name, { required: true })}
+        placeholder={`${text}을 입력하십시오`}
+      />
+      {errorMsg && <span>{errorMsg}</span>}
     </Wrapper>
   );
 }
@@ -21,7 +37,8 @@ const Wrapper = styled.div`
   margin-bottom: 20px;
   label {
     color: #aaa;
-    font-size: 18px;
+    font-size: 15px;
+    margin-bottom: 5px;
   }
   input {
     display: block;
@@ -29,5 +46,9 @@ const Wrapper = styled.div`
     padding: 15px 20px;
     border-radius: 20px;
     background: rgba(255, 255, 255, 1);
+  }
+  span {
+    margin-top: 3px;
+    color: red;
   }
 `;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,8 +3,27 @@ import { BiSolidTennisBall } from "react-icons/bi";
 import { Link } from "react-router-dom";
 import AuthInput from "../components/AuthInput";
 import HomeLayout from "../layouts/HomeLayout";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { signInWithEmailAndPassword } from "@firebase/auth";
+import { auth } from "../firebase/firebase";
+import { useNavigate } from "react-router-dom";
+
+export interface ILoginValue {
+  email: string;
+  password: string;
+}
 
 export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<ILoginValue>();
+  const navigate = useNavigate();
+
+  const onSubmitHandler: SubmitHandler<ILoginValue> = async (data) => {};
+
   return (
     <HomeLayout>
       <Logo>
@@ -13,11 +32,23 @@ export default function Login() {
       </Logo>
       <Box>
         <Title>로그인</Title>
-        <div>
-          <AuthInput id="id" name="아이디" />
-          <AuthInput id="password" name="비밀번호" />
-          <SubmitBtn>로그인</SubmitBtn>
-        </div>
+        <form onSubmit={handleSubmit(onSubmitHandler)}>
+          <AuthInput
+            name="email"
+            text="이메일"
+            inputType="email"
+            register={register}
+            errorMsg={errors.email && "이메일을 입력해주세요"}
+          />
+          <AuthInput
+            name="password"
+            text="비밀번호"
+            inputType="password"
+            register={register}
+            errorMsg={errors.password && "비밀번호를 입력해주세요"}
+          />
+          <SubmitBtn type="submit">로그인</SubmitBtn>
+        </form>
         <hr />
         <Others>
           <div>아직 회원이 아니신가요?</div>
@@ -43,7 +74,7 @@ const Logo = styled.div`
 const Box = styled.div`
   position: relative;
   width: 400px;
-  height: 400px;
+  min-height: 400px;
   border-radius: 30px;
   padding: 30px 70px;
   background-color: rgb(255, 255, 255, 0.5);
@@ -56,8 +87,9 @@ const Title = styled.div`
   margin-bottom: 10px;
 `;
 
-const SubmitBtn = styled.div`
-  margin: 30px 0px;
+const SubmitBtn = styled.button`
+  width: 100%;
+  margin: 20px 0px 30px 0px;
   box-sizing: border-box;
   appearance: none;
   background-color: #9bc940;
